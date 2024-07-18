3a8fda1f01ae64a5c5b86151f613be403d5762b4
nishc <nishchai@outlook.com>
2024-07-18 23:13:02 +0100

    experimenting with diff topbars

--- lib/common_widgets.dart
+++ lib/common_widgets.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class CommonWidgets {
   static const String motifImageURI =
-      "https://img.freepik.com/free-vector/abstract-blue-tone-memphis-patterned-social-template-vector_53876-140327.jpg";
+      "https://img.freepik.com/free-vector/paper-style-smooth-background_23-2148983444.jpg?w=2000&t=st=1721240805~exp=1721241405~hmac=ae423767b8d2cd153f2724d8a71310a975b1dafceb7a8b4e3d4fee21d345cc44";
+  //    "https://img.freepik.com/free-vector/abstract-blue-tone-memphis-patterned-social-template-vector_53876-140327.jpg";
 
   static Container topBanner() {
     return Container(
