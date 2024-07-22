ce2a8a18c6f93a590a1b4526a06d7c6410c9d1de
nishc <nishchai@outlook.com>
2024-07-22 11:41:53 -0400

    Remove unused imports in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-// ignore: unused_import
-import 'login_screen/public_speak_login.dart';
-// ignore: unused_import
-import 'home_screen/public_speak_home.dart';
-// ignore: unused_import
-import 'recordings_screen/public_speak_recordings.dart';
 import 'speak_up_app.dart';
 
 void main() {
