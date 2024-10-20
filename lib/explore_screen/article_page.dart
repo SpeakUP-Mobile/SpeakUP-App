ae64a91395ca7bbf25ad639bd64153d843b1b643
clcanvas <nishchai@outlook.com>
2024-10-20 15:53:41 -0400

    Article system implemented

---
+++ lib/explore_screen/article_page.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:get/get.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:speakup/explore_screen/explore_controller.dart';
+
+class ArticlePage extends GetView<ExploreController> {
+  const ArticlePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Get.lazyPut(() => ExploreController());
+    return Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 100,
+          title: Container(
+            margin: const EdgeInsets.only(right: 45),
+            child: Text(
+              Get.arguments[0],
+              softWrap: true,
+              overflow: TextOverflow.visible,
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          scrolledUnderElevation: 0,
+        ),
+        body: SafeArea(
+          bottom: false,
+          child: Container(
+            margin: const EdgeInsets.only(left: 20, right: 30),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    Get.arguments[1],
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(fontSize: 16),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                    onTap: () => Get.back(),
+                    child: Container(
+                      height: 50,
+                      width: 150,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.only(bottom: 1),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: const [
+                            BoxShadow(
+                              color: Color.fromARGB(50, 0, 0, 0),
+                              spreadRadius: 0.5,
+                              blurRadius: 1,
+                            )
+                          ]),
+                      child: const Text(
+                        textAlign: TextAlign.center,
+                        "i'm done reading",
+                        style: TextStyle(
+                            color: Color(0xFF3A0E67),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
--- lib/explore_screen/article_widget.dart
+++ lib/explore_screen/article_widget.dart
@@ -15,37 +15,45 @@ class ArticleWidget extends GetView<ExploreController> {
 
   @override
   Widget build(BuildContext context) {
+    Get.lazyPut(
+      () => ExploreController(),
+    );
+
     return GestureDetector(
+        onTap: () => controller.viewArticle(name, content),
         child: Container(
-      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
-      padding: const EdgeInsets.only(left: 10, top: 10),
-      height: 145,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(imgURI),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-              Colors.black.withOpacity(0.6), // Adjust the opacity to darken
-              BlendMode.darken, // This blends the image with black to darken it
-            ),
-          ),
-          borderRadius: const BorderRadius.all(Radius.circular(8.0)),
-          color: Colors.white,
-          boxShadow: const [
-            BoxShadow(
-              color: Color.fromARGB(50, 0, 0, 0),
-              spreadRadius: 0,
-              blurRadius: 4,
-            )
-          ]),
-      child: Container(
-          margin: const EdgeInsets.only(bottom: 10, left: 10, right: 20),
-          alignment: Alignment.centerLeft,
-          child: Text(
-            name,
-            style: const TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 24),
-          )),
-    ));
+          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+          padding: const EdgeInsets.only(left: 10, top: 10),
+          height: 145,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(imgURI),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.6), // Adjust the opacity to darken
+                  BlendMode
+                      .darken, // This blends the image with black to darken it
+                ),
+              ),
+              borderRadius: const BorderRadius.all(Radius.circular(8.0)),
+              color: Colors.white,
+              boxShadow: const [
+                BoxShadow(
+                  color: Color.fromARGB(50, 0, 0, 0),
+                  spreadRadius: 0,
+                  blurRadius: 4,
+                )
+              ]),
+          child: Container(
+              margin: const EdgeInsets.only(bottom: 10, left: 10, right: 20),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                name,
+                style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24),
+              )),
+        ));
   }
 }
